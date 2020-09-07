eeea084602d9a150545e0ec8d5365e9dd4192703
edmundgjj <edmundgjj@gmail.com>
2020-09-07 21:44:27 +0800

    cors

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 $(function(){
 
-    let url = 'https://instagram-2698.restdb.io/userdata?key=5f50da4dc5e01c1e033b8be4';
+    let url = 'https://cors-anywhere.herokuapp.com/https://instagram-2698.restdb.io/userdata?key=5f50da4dc5e01c1e033b8be4';
   
     $('#submitInstagram').on('click',function(e){
       e.preventDefault();
